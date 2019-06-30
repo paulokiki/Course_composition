89a96c52ba42b1bba7551b62c392067860ad2ced
paulokiki <vieiralinspaulo@gmail.com>
2019-06-30 14:52:57 -0300

    iniciando programa

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,35 @@
 package application;
 
+import com.sun.corba.se.spi.orbutil.threadpool.Work;
+import entities.Department;
+import entities.Worker;
+import entities.enums.WorkLevel;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
+        
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter department's name: ");
+        String departmentName = sc.nextLine();
+        
+        System.out.println("Enter work date:");
+        System.out.print("Name: ");
+        String workerName = sc.nextLine();
+        
+        System.out.print("Level: ");
+        String workerLevel = sc.nextLine();
+        
+        System.out.print("Salary: ");
+        Double baseSalary = sc.nextDouble();
+        
+        Worker worker = new Worker(workerName, WorkLevel.valueOf(workerName), baseSalary, new Department(departmentName) );
+        
+               
     }
 
 }
--- src/entities/enums/WorkLevel.java
+++ src/entities/enums/WorkLevel.java
@@ -2,4 +2,9 @@ package entities.enums;
 
 public enum WorkLevel {
     JUNIOR, MID_LEVEL, SENIOR;
+
+    private WorkLevel() {
+    }
+    
+    
 }
